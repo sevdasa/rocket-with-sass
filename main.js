d14eff19c3173b5a91aebe69eb7ac01b7dd62c6d
sevda <sevdasazdar@gmail.com>
2020-05-06 11:09:20 +0430

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+
+  function opend() {
+    var content = document.getElementById("hambergerMenu-content");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+  
+
